777c6e73a2cd650977541b9012a1e704330f652f
giovanni-abundio <giovanni@abundioenergy.com>
2025-10-20 16:34:57 +0200

    feat: adding more configs for hrms frontend

---
+++ src/libs/Env.ts
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const Env = createEnv({
+  server: {},
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().optional(),
+  },
+  shared: {
+    NODE_ENV: z.enum(["test", "development", "production"]).optional(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+});
---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
+import createMiddleware from "next-intl/middleware";
+
+import { routing } from "./libs/I18nRouting";
+
+const handleI18nRouting = createMiddleware(routing);
+
+export default async function middleware(
+  request: NextRequest,
+  _: NextFetchEvent
+) {
+  return handleI18nRouting(request);
+}
+
+export const config = {
+  // Match all pathnames except for
+  // - … if they start with `/_next`, `/_vercel` or `monitoring`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!_next|_vercel|monitoring|.*\\..*).*)",
+  runtime: "nodejs",
+};
